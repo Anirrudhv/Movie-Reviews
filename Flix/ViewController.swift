c68a1a03da11a559cabd0afb7234c26fd4a9c216
Anirudh V <anirudhv@Anirudhs-MacBook-Pro.local>
2018-07-28 20:25:10 -0700

    Part1 completed

--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 import AlamofireImage
-class ViewController: UIViewController,UITableViewDataSource {
+class ViewController: UIViewController,UITableViewDataSource,UISearchBarDelegate{
+    
+    @IBOutlet weak var ac: UIActivityIndicatorView!
+    
+    var refreshcontrol:UIRefreshControl!
+    var issearching = false
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return movies.count
+    
+        return movies.count
+       
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -26,6 +33,10 @@ class ViewController: UIViewController,UITableViewDataSource {
         let baseURL = "https://image.tmdb.org/t/p/w500"
         let posterURL = URL(string: baseURL+posterpathstring)!
         cell.PosterImage.af_setImage(withURL: posterURL)
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.gray
+        cell.selectedBackgroundView = backgroundView
+        
         
         return cell
     }
@@ -34,17 +45,33 @@ class ViewController: UIViewController,UITableViewDataSource {
     
     var movies: [[String:Any]] = []
     
-   
+    var filtereddata = [String]()
     
 
     @IBOutlet weak var TableView: UITableView!
     override func viewDidLoad() {
         
-        TableView.estimatedRowHeight = UITableViewAutomaticDimension
-        TableView.rowHeight = UITableViewAutomaticDimension
-        
+        //TableView.estimatedRowHeight = UITableViewAutomaticDimension
+        TableView.rowHeight = 150
+        refreshcontrol = UIRefreshControl()
+        refreshcontrol.addTarget(self, action: #selector
+            (ViewController.didpullrequest(_refreshcontrol:)), for: .valueChanged)
+        TableView.insertSubview(refreshcontrol, at: 0)
+       
         TableView.dataSource = self
         super.viewDidLoad()
+        ac.startAnimating()
+
+        networkRequest()
+    
+    }
+    
+
+    
+    @objc func didpullrequest(_refreshcontrol: UIRefreshControl){
+        networkRequest()
+    }
+    func networkRequest(){
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -52,25 +79,36 @@ class ViewController: UIViewController,UITableViewDataSource {
             // This will run when the network request returns
             if let error = error {
                 print(error.localizedDescription)
+                self.createAlert(title : "Network ERROR", message : "PLease connect to the internet")
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String:Any]]
                 self.movies = movies
                 self.TableView.reloadData()
-               
+                self.ac.stopAnimating()
+                self.refreshcontrol.endRefreshing()
+                
+               // DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                //  self.ac.isHidden = true
+              //  }
+                
                 
             }
         }
         task.resume()
-      
-    
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func createAlert(title : String, message : String){
+        let Alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        Alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {(action) in Alert.dismiss(animated: true, completion: nil)}))
+        self.present(Alert, animated: true, completion: nil)
+    }
+   
+    
+    
+    
 }
 
